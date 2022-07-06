454f171abc61da1e888b004c034c25e47427e804
Laman <laman_garayeva@outlook.com>
2022-07-06 19:48:50 +0200

    initial commit

--- src/test/java/com/centrilli/pages/BasePage.java
+++ src/test/java/com/centrilli/pages/BasePage.java
@@ -1,6 +1,8 @@
 package com.centrilli.pages;
 
 import com.centrilli.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
@@ -9,5 +11,7 @@ public class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(xpath = "//a[@data-menu='120']/span[contains(text(), 'Calendar')]")
+    public WebElement calendarModule;
 
 }
