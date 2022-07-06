c4735aff5c2f6f4b6c332b1526f098453ed5c4cc
Musa Emre Yagci <emre@meyagci.com>
2022-07-07 02:02:30 +0800

    Methods rearranged and new lines added for better readibility

--- src/test/java/com/centrilli/step_defs/Employees_StepDefs.java
+++ src/test/java/com/centrilli/step_defs/Employees_StepDefs.java
@@ -26,70 +26,73 @@ public class Employees_StepDefs {
     public void user_is_logged_in_as_pos_manager() {
         loginPage.login();
     }
+
     @Given("user is on Employees page")
     public void user_is_on_employees_page() {
         employeesPage.btnNavigationEmployees.click();
     }
 
-
     @When("user clicks Create button")
     public void user_clicks_create_button() {
         employeesPage.btnCreate.click();
     }
-    @Then("employee creation page shows up")
-    public void employee_creation_page_shows_up() {
-        Assert.assertTrue(employeesPage.textNewEmployee.isDisplayed());
-    }
-
 
     @When("user clicks Kanban button")
     public void user_clicks_kanban_button() {
         employeesPage.btnSwitchKanbanView.click();
     }
-    @Then("employees are listed as grid")
-    public void employees_are_listed_as_grid() {
-        Assert.assertTrue(employeesPage.kanbanViewEmployees.isDisplayed());
-    }
-
 
     @When("user clicks List button")
     public void user_clicks_list_button() {
         employeesPage.btnSwitchListView.click();
     }
-    @Then("employees are listed as a list")
-    public void employees_are_listed_as_a_list() {
-        Assert.assertTrue(employeesPage.listViewEmployees.isDisplayed());
-    }
-
 
     @When("user fills in Name field")
     public void fills_in_name_field() {
         employeesPage.inputName.sendKeys("SomeName");
     }
-    @And("user clicks Save button")
+
+    @When("user clicks Save button")
     public void clicks_save_button() {
         employeesPage.btnSave.click();
     }
+
+    @When("user types in a name to Name field")
+    public void user_types_in_to_name_field() {
+        employeesPage.inputName.sendKeys(employeeName);
+    }
+
+    @When("user clicks the Employees module")
+    public void user_clicks_the_employees_module() {
+        employeesPage.btnNavigationEmployees.click();
+    }
+
+    @When("user searches the name on the search box")
+    public void user_types_in_to_search_box() {
+        wait.until(ExpectedConditions.visibilityOf(employeesPage.inputSearchBox));
+        employeesPage.inputSearchBox.sendKeys(employeeName + Keys.ENTER);
+    }
+
+    @Then("employee creation page shows up")
+    public void employee_creation_page_shows_up() {
+        Assert.assertTrue(employeesPage.textNewEmployee.isDisplayed());
+    }
+
+    @Then("employees are listed as grid")
+    public void employees_are_listed_as_grid() {
+        Assert.assertTrue(employeesPage.kanbanViewEmployees.isDisplayed());
+    }
+
+    @Then("employees are listed as a list")
+    public void employees_are_listed_as_a_list() {
+        Assert.assertTrue(employeesPage.listViewEmployees.isDisplayed());
+    }
+
     @Then("'Employee created' message appears under full profile")
     public void employee_created_message_appears_under_full_profile() {
         Assert.assertTrue(employeesPage.textEmployeeCreated.isDisplayed());
     }
 
-
-
-    @And("user types in a name to Name field")
-    public void user_types_in_to_name_field() {
-        employeesPage.inputName.sendKeys(employeeName);
-    }
-    @And("user clicks the Employees module")
-    public void user_clicks_the_employees_module() {
-        employeesPage.btnNavigationEmployees.click();
-    }
-    @And("user searches the name on the search box")
-    public void user_types_in_to_search_box() {
-        wait.until(ExpectedConditions.visibilityOf(employeesPage.inputSearchBox));
-        employeesPage.inputSearchBox.sendKeys(employeeName + Keys.ENTER);
-    }
     @Then("employee is listed in the search result")
     public void employee_is_listed() {
         Assert.assertTrue(employeesPage.getSearchedEmployee(employeeName).isDisplayed());
@@ -99,4 +102,5 @@ public class Employees_StepDefs {
     public void name_field_is_highlighted() {
         Assert.assertTrue(employeesPage.inputNameWithInvalidHighlight.isDisplayed());
     }
+
 }
