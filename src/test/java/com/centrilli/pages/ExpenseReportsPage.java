9d3a7d01006ca58a8efbdb623baf81615d8be9f9
Musa Emre Yagci <emre@meyagci.com>
2022-07-16 07:00:16 +0800

    ExpensesPage renamed to ExpenseReportsPage and elements created

---
+++ src/test/java/com/centrilli/pages/ExpenseReportsPage.java
@@ -0,0 +1,33 @@
+package com.centrilli.pages;
+
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ExpensesPage {
+
+    public ExpensesPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//a[@data-menu-xmlid='hr_expense.menu_hr_expense_sheet_my_reports']")
+    WebElement btnExpenseReports;
+
+    @FindBy(xpath = "//button[contains(text(), 'Create')]")
+    WebElement btnCreate;
+
+    @FindBy(xpath = "//input[@placeholder='e.g. Trip to NY']")
+    WebElement inputExpenseReportSummary;
+
+    @FindBy(xpath = "//div[@name='employee_id']//span[@class='o_dropdown_button']")
+    WebElement dropdownBtnEmployee;
+
+    @FindBy(xpath = "//p[text()='Expense Report created']")
+    WebElement textExpenseReportCreated;
+
+    @FindBy(xpath = "//li[@class='ui-menu-item'][1]")
+    WebElement firstEmployeeItem;
+
+}
