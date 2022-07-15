42c273080d79759aef2095e1ded4f33ce9efe470
Musa Emre Yagci <emre@meyagci.com>
2022-07-16 07:37:13 +0800

    Step defs for AC "Verify that the Expense Manager is able to create Expenses Report Summary by selecting 'Create' option for each Employee." created

--- src/test/java/com/centrilli/step_defs/Expenses_StepDefs.java
+++ src/test/java/com/centrilli/step_defs/Expenses_StepDefs.java
@@ -1,40 +1,47 @@
 package com.centrilli.step_defs;
 
-import com.centrilli.pages.BasePage;
-import io.cucumber.java.en.Given;
+import com.centrilli.pages.ExpenseReportsPage;
+import com.centrilli.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Expenses_StepDefs {
+    ExpenseReportsPage expenseReportsPage = new ExpenseReportsPage();
 
     @When("user clicks Expense Reports button")
     public void user_clicks_expense_reports_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        expenseReportsPage.btnExpenseReports.click();
     }
 
     @When("user clicks Create button in Expense Reports")
     public void user_clicks_create_button_in_expense_reports() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        new WebDriverWait(Driver.getDriver(), 20).until(ExpectedConditions.titleContains("My Reports"));
+        expenseReportsPage.btnCreate.click();
     }
 
     @When("user fills in Expense Report Summary field")
     public void user_fills_in_expense_report_summary_field() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        expenseReportsPage.inputExpenseReportSummary.sendKeys("Test Summary Title");
     }
 
     @When("user selects an employee")
     public void user_selects_an_employee() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        expenseReportsPage.dropdownInputEmployee.click();
+        expenseReportsPage.dropdownItemFirstEmployee.click();
+    }
+
+    @And("user clicks Save button in Expense Reports")
+    public void user_clicks_save_button_in_expense_reports() {
+        expenseReportsPage.btnSave.click();
     }
 
     @Then("'Expense Report created' message appears")
     public void message_appears() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(expenseReportsPage.textExpenseReportCreated.isDisplayed());
     }
 
 }
