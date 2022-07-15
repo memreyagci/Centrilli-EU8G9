b0c00b153401866f0eba734ac99fbb1e93cd4e0a
Musa Emre Yagci <emre@meyagci.com>
2022-07-15 23:18:28 +0800

    new method for clicking navigation bar buttons is added

--- src/test/java/com/centrilli/pages/BasePage.java
+++ src/test/java/com/centrilli/pages/BasePage.java
@@ -1,6 +1,7 @@
 package com.centrilli.pages;
 
 import com.centrilli.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -11,7 +12,23 @@ public class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    public void clickNavBarBtn(String btnStr) {
+        WebElement navBarBtn = Driver.getDriver().findElement(By.xpath("//nav//span[contains(text(), '" + btnStr + "')]/.."));
+
+        // If navBarBtn is not displayed, it's in More menu because of reasons related to screen size, then it needs to be clicked first.
+        if (navBarBtn.isDisplayed()) {
+            navBarBtn.click();
+        } else {
+            this.dropdownMenuMore.click();
+            navBarBtn.click();
+        }
+    }
+
+
     @FindBy(xpath = "//a[@data-menu='120']/span[contains(text(), 'Calendar')]")
     public WebElement calendarModule;
+
+    @FindBy(id = "menu_more_container")
+    public WebElement dropdownMenuMore;
 
 }
