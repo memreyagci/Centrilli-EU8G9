1e2c0ce467a9edb5a9ae6e7ddee25e51f9c108f4
Musa Emre Yagci <emre@meyagci.com>
2022-07-19 04:09:46 +0800

    A method and its implementation for clicking submenu is added

--- src/test/java/com/centrilli/pages/BasePage.java
+++ src/test/java/com/centrilli/pages/BasePage.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
+
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -23,6 +27,17 @@ public class BasePage {
             navBarBtn.click();
         }
 
+    }
+
+    public void clickSubMenuBtn(String subMenuName) {
+        String currentTitle = Driver.getDriver().getTitle();
+        WebElement subMenuBtn = Driver.getDriver().findElement(By.xpath("//div[@class='o_sub_menu_content']//span[contains(text(), '" + subMenuName + "')]/.."));
+        subMenuBtn.click();
+
+        /* It takes a while for submenu page to load, so this is necessary to not get an error.
+        You can't simply check what title is, because it does not always match with button name itself.
+        Thus, we check whether it is different from prior to clicking. */
+        wait.until(ExpectedConditions.not(ExpectedConditions.titleIs(currentTitle)));
     }
 
 
