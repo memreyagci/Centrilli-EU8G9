d3638327bb33f2c9d7a062438ecac93defec8430
arlind00 <aarlind@live.com>
2022-07-21 07:36:45 +0200

    newContact 4th commit

--- src/test/java/com/centrilli/pages/NewContactPage.java
+++ src/test/java/com/centrilli/pages/NewContactPage.java
@@ -1,4 +1,31 @@
 package com.centrilli.pages;
 
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class NewContactPage {
+
+    public NewContactPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
