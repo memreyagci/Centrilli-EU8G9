6fea7c6ee54c9aadbace3d03c1471c4e40f65d08
merve <merverdem87@gmail.com>
2022-07-22 19:33:10 +0200

    NotesCreated

--- src/test/java/com/centrilli/step_defs/CreateNotes_StepDefs.java
+++ src/test/java/com/centrilli/step_defs/CreateNotes_StepDefs.java
@@ -1,21 +1,25 @@
 package com.centrilli.step_defs;
 
 import com.centrilli.pages.CreateNotesPage;
+import com.centrilli.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class
 CreateNotes_StepDefs {
 
     CreateNotesPage createNotesPage = new CreateNotesPage();
-
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
     @When("User clicks Notes button")
     public void user_clicks_notes_button() {
         createNotesPage.notesButton.click();
     }
+
     @And("User clicks on Create button")
     public void user_clicks_on_create_button() {
         createNotesPage.createButton.click();
@@ -34,54 +38,52 @@ CreateNotes_StepDefs {
         Assert.assertTrue(createNotesPage.elementP.isDisplayed());
     }
 
-
     @When("User clicks Kanban button")
     public void user_clicks_kanban_button() {
-
+        createNotesPage.kanbanButton.click();
     }
     @Then("User clicks List button")
     public void user_clicks_list_button() {
-
+        createNotesPage.listButton.click();
     }
 
     @When("User clicks Discard button")
     public void user_clicks_discard_button() {
-
+        createNotesPage.discardButton.click();
     }
     @Then("Verify user can discard notes")
     public void verify_user_can_discard_notes() {
-
+        createNotesPage.warningMessageConfirmation.click();
     }
 
-    @When("User selects one of the created Notes")
+    @When("User selects one of the created Notes from list in Notes")
     public void user_selects_one_of_the_created_notes() {
-
+         createNotesPage.listButton.click();
+         createNotesPage.note.click();
     }
+
     @When("User clicks Edit button")
     public void user_clicks_edit_button() {
+         createNotesPage.editButton.click();
+         createNotesPage.textBox.sendKeys("edited");
 
     }
 
     @Then("Verify User can edited notes")
     public void verify_user_can_edited_notes() {
-
+         Assert.assertTrue(createNotesPage.textMessage.isDisplayed());
     }
 
-
-    @When("User selects one of the created Notes from radio button")
-    public void user_selects_one_of_the_created_notes_from_radio_button() {
-
-    }
     @When("User clicks action button")
     public void user_clicks_action_button() {
-
+        wait.until(ExpectedConditions.visibilityOf(createNotesPage.actionButton));
+        createNotesPage.actionButton.click();
     }
-    @When("User selects Delete button")
-    public void user_selects_delete_button() {
 
-    }
     @Then("Verify User deleted selected notes")
     public void verify_user_deleted_selected_notes() {
+        createNotesPage.deleteButton.click();
+        createNotesPage.okButton.click();
 
     }
 
