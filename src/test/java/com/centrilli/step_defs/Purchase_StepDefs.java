9a999663d6ea35f298d154611e3685ecd5c0314a
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-07-22 17:23:57 +0200

    Update purchase feature, stepsdef,page

--- src/test/java/com/centrilli/step_defs/Purchase_StepDefs.java
+++ src/test/java/com/centrilli/step_defs/Purchase_StepDefs.java
@@ -20,7 +20,9 @@ public class Purchase_StepDefs {
 
     @When("user enters Vendor name")
     public void user_enters_vendor_name() {
-    purchasePage.vendorBox.sendKeys("Test"+ Keys.ENTER);
+        purchasePage.vendorBox.click();
+        wait.until(ExpectedConditions.elementToBeClickable(purchasePage.vendorBox));
+        purchasePage.vendorName.click();
 
     }
     @Then("user sees expected Draft Bill title")
@@ -32,7 +34,7 @@ public class Purchase_StepDefs {
 
     @Then("user can switch Kanban view")
     public void user_can_switch_kanban_view() {
-        BrowserUtils.sleep(3);
+        wait.until(ExpectedConditions.urlContains("kanban"));
         BrowserUtils.verifyURLContains("kanban");
     }
     @Then("user can switch List view")
@@ -45,6 +47,7 @@ public class Purchase_StepDefs {
 
     @Then("user returns Vendor Bills page")
     public void user_returns_vendor_bills_page() {
+        purchasePage.okButton.click();
         wait.until(ExpectedConditions.visibilityOf(purchasePage.vendorBillsTitle));
         Assert.assertTrue(purchasePage.vendorBillsTitle.isDisplayed());
     }
@@ -53,23 +56,44 @@ public class Purchase_StepDefs {
 
     @When("user changes Vendor name")
     public void user_changes_vendor_name() {
-    purchasePage.vendorBox.clear();
-    purchasePage.vendorBox.sendKeys("Name Changed"+Keys.ENTER);
+        purchasePage.vendorBox.clear();
+        purchasePage.vendorBox.sendKeys("Name Changed"+Keys.ENTER);
     }
     @Then("Vendor name changes")
     public void vendor_name_changes() {
-    Assert.assertTrue(purchasePage.vendorName.isDisplayed());
+        Assert.assertTrue(purchasePage.newVendorName.isDisplayed());
     }
 
 
 
-    @When("user clicks Action button")
-    public void user_clicks_action_button() {
+    @When("user deletes Vendor Bill")
+    public void user_deletes_vendor_bill() {
+        BrowserUtils.sleep(3);
+        purchasePage.actionsDropdown.click();
+        wait.until(ExpectedConditions.visibilityOf(purchasePage.deleteButton));
+        purchasePage.deleteButton.click();
+        purchasePage.okButton.click();
 
     }
     @Then("Vendor Bills is deleted")
     public void vendor_bills_is_deleted() {
+        wait.until(ExpectedConditions.textToBePresentInElement(purchasePage.vendorCount, "80"));
+        Assert.assertEquals("80", purchasePage.vendorCount.getText());
 
+
+
+    }
+    @Then("The following fields are invalid message is displayed")
+    public void the_following_fields_are_invalid_message_is_displayed() {
+        purchasePage.vendorReferance.sendKeys("1234567");
+        wait.until(ExpectedConditions.visibilityOf(purchasePage.errorMessage));
+        Assert.assertTrue(purchasePage.errorMessage.isDisplayed());
+
+
+    }
+    @Then("user clicks Save button")
+    public void user_save_button() {
+        purchasePage.save.click();
     }
 
 }
